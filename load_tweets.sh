8516fb617cf8160b6501d53165817453d07ac250
kjiang25 <kjiang25@cmc.edu>
2025-04-09 11:39:13 -0700

    fixed load_tweets.sh

--- load_tweets.sh
+++ load_tweets.sh
@@ -14,11 +14,11 @@ test-data.zip
     # call the load_tweets.py file to load data into pg_normalized
  done
 
-# echo 'load denormalized'
-# for file in $files; do
-   #  unzip -p "$file" | sed 's/\\u0000//g' | psql postgres://postgres:pass@localhost:10651/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+ echo 'load denormalized'
+ for file in $files; do
+     unzip -p "$file" | sed 's/\\u0000//g' | psql postgres://postgres:pass@localhost:10651/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
     # use SQL's COPY command to load data into pg_denormalized
-# done
+ done
 
 # Notes
 # - Looping over $files, which only has a single file
